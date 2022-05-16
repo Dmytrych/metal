d3f34caf753fb321ec1fb6964e6ebc461e4dcf2e
Dmytrych <55029343+Dmytrych@users.noreply.github.com>
2022-05-16 14:04:28 +0300

    Added basics

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import "./App.css"
+import { useContext, useState } from "react";
+import Calculator from "./Pages/Calculator";
 
 function App() {
+  const [ menuVisible, setMenuVisible] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/" element={<Calculator/>} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
